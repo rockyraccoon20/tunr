7d596c5e4dc6b106ebc72ad9947665040776f8a6
rockyraccoon20 <tim_montgomery20@yahoo.com>
2014-10-20 12:05:06 -0400

    Test for no albums available
    created a test for no albums available

--- spec/managing_albums_spec.rb
+++ spec/managing_albums_spec.rb
@@ -5,6 +5,7 @@ feature "Manage Albums" do
     visit albums_path
     expect(page.find('h1')).to have_content (/Albums/)
     expect(page).to have_content(/No albums available/i)
+
 end
     scenario "List all Albums" do
         Album.create!(
@@ -17,7 +18,9 @@ end
     expect(page.find('.album')).to have_content(/Astrolounge/)
     expect(page.find('.artist')).to have_content(/Smash Mouth/)
     expect(page.find('.year')).to have_content(/1999/)
+    expect(page).not_to have_content (/No albums available/i)
 end
 
     scenario "Add new Album"
+
 end
